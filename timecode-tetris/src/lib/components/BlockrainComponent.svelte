e84e68c7f8cd3e2f6b46406009ebb179951beea1
Exr0n <howdy@exr0n.com>
2024-01-25 11:08:02 -0800

    blockrain imports pointing to the correct place

---
+++ timecode-tetris/src/lib/components/BlockrainComponent.svelte
@@ -0,0 +1,11 @@
+<script>
+
+
+    import "$lib/blockrain-assets/js/jquery-1.11.1.min.js";
+    import "$lib/blockrain-dist/blockrain.jquery.min.js";
+    import "$lib/blockrain-dist/blockrain.css";
+
+    jq('.game').blockrain();
+</script>
+
+<div class="game border-2 border-blue-300" style="width:250px; height:500px;"></div>
---
+++ timecode-tetris/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Blockrain from "$lib/components/BlockrainComponent.svelte";
+</script>
+
+<body>
+    <Blockrain />
+</body>
