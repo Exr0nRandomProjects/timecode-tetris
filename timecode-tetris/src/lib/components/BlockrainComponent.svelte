62e97021b33f1799a09f69754de588d05b28a638
Exr0n <howdy@exr0n.com>
2024-01-25 11:13:05 -0800

    omg playable tetris!!

--- timecode-tetris/src/lib/components/BlockrainComponent.svelte
+++ timecode-tetris/src/lib/components/BlockrainComponent.svelte
@@ -1,11 +1,13 @@
 <script>
+import { onMount } from 'svelte';
 
+onMount(async () => {
+    const jq = await import("$lib/blockrain-assets/js/jquery-1.11.1.min.js");
+    await import("$lib/blockrain-dist/blockrain.jquery.min.js");
+    await import("$lib/blockrain-dist/blockrain.css");
 
-    import "$lib/blockrain-assets/js/jquery-1.11.1.min.js";
-    import "$lib/blockrain-dist/blockrain.jquery.min.js";
-    import "$lib/blockrain-dist/blockrain.css";
-
-    jq('.game').blockrain();
+    jQuery('.game').blockrain();
+})
 </script>
 
 <div class="game border-2 border-blue-300" style="width:250px; height:500px;"></div>
