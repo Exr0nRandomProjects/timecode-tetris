887183696b73a1f2ffacd2eabaac2fd8b88aeec7
Exr0n <howdy@exr0n.com>
2024-01-25 17:52:21 -0800

    use svelte-chartjs but it needs to be updated

--- timecode-tetris/src/lib/components/BlockrainComponent.svelte
+++ timecode-tetris/src/lib/components/BlockrainComponent.svelte
@@ -1,13 +1,38 @@
 <script>
 import { onMount } from 'svelte';
+import { Scatter } from 'svelte-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement, } from 'chart.js';
+ChartJS.register( Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
+
+let moments = {
+    key: [],
+    blockappear: [],
+    placed: [],
+    line: []
+}
 
 onMount(async () => {
-    const jq = await import("$lib/blockrain-assets/js/jquery-1.11.1.min.js");
-    await import("$lib/blockrain-dist/blockrain.jquery.min.js");
-    await import("$lib/blockrain-dist/blockrain.css");
+    await import("$lib/blockrain-assets/js/jquery-1.11.1.min.js");
+    await Promise.all([
+        import("$lib/blockrain-dist/blockrain.jquery.min.js"),
+        import("$lib/blockrain-dist/blockrain.css"),
+    ]);
 
-    jQuery('.game').blockrain({ onKey: console.log, onBlockAppear: console.log });
-})
+    jQuery('.game').blockrain({
+        onKey: () => { moments.key.push(Date.now()) },
+        onBlockAppear: () => { moments.blockappear.push(Date.now()) },
+        placed: () => { moments.placed.push(Date.now()) },
+        line: () => { moments.line.push(Date.now()) },
+    });
+});
+
+const scatter_data = {
+    datasets: Object.entries(moments).map(([k, v], i) => ({
+        label: k,
+        data: v.map(t => ({ x: t, y: i }))
+    }))
+}
 </script>
 
-<div class="game border-2 border-blue-300 h-full aspect-[1/2] m-auto"></div>
+<div class="game border-2 border-blue-300 h-full aspect-{1/2} m-auto"></div>
+<Scatter data={scatter_data} options={{ responsive: true }} />
