cb2a9549342360c4cf8391dc817e193b8f82f7c1
Exr0n <howdy@exr0n.com>
2024-01-26 01:24:04 -0800

    chart barely visible but technically working

--- timecode-tetris/src/lib/components/BlockrainComponent.svelte
+++ timecode-tetris/src/lib/components/BlockrainComponent.svelte
@@ -1,15 +1,10 @@
 <script>
 import { onMount } from 'svelte';
-import { Scatter } from 'svelte-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement, } from 'chart.js';
 ChartJS.register( Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
 
-let moments = {
-    key: [],
-    blockappear: [],
-    placed: [],
-    line: []
-}
+export let callback_names;
+export let event_callback;
 
 onMount(async () => {
     await import("$lib/blockrain-assets/js/jquery-1.11.1.min.js");
@@ -18,21 +13,19 @@ onMount(async () => {
         import("$lib/blockrain-dist/blockrain.css"),
     ]);
 
+    const callbacks = Object.fromEntries(callback_names.map((k, i) =>
+        ['on' + k, () => {
+            // scatter_data[i]?.data?.push(Date.now());
+            event_callback(i, Date.now());
+        }]
+    ))
+
     jQuery('.game').blockrain({
-        onKey: () => { moments.key.push(Date.now()) },
-        onBlockAppear: () => { moments.blockappear.push(Date.now()) },
-        placed: () => { moments.placed.push(Date.now()) },
-        line: () => { moments.line.push(Date.now()) },
+        ...callbacks
     });
 });
 
-const scatter_data = {
-    datasets: Object.entries(moments).map(([k, v], i) => ({
-        label: k,
-        data: v.map(t => ({ x: t, y: i }))
-    }))
-}
+
 </script>
 
 <div class="game border-2 border-blue-300 h-full aspect-{1/2} m-auto"></div>
-<Scatter data={scatter_data} options={{ responsive: true }} />
--- timecode-tetris/src/routes/+page.svelte
+++ timecode-tetris/src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <script>
+    import { Scatter } from 'svelte-chartjs';
+
     import Blockrain from "$lib/components/BlockrainComponent.svelte";
     let charts_shown = true;
+
+
+    const callback_names = ['Key', 'BlockAppear', 'Placed', 'Line'];
+
+    $: scatter_data = {
+        datasets: callback_names.map((k, i) => ({
+            label: k,
+            data: [],
+        }))
+    }
+
+    let chart;
+    const event_callback = (idx, time) => {
+        scatter_data.datasets[idx]?.data?.push({ x: time, y: idx });
+        chart.update();
+    }
 </script>
 
 <body class="overscroll-none h-screen w-screen fixed flex justify-center space-x-12">
     <div class="h-screen w-1/3">
-        <Blockrain />
+        <Blockrain event_callback={event_callback} callback_names={callback_names} />
     </div>
     <div class="flex-col">
         <div class="">
@@ -15,7 +33,7 @@
             </span>
             {#if charts_shown}
                 <div class="border-1- border-red-100 w-[30rem] h-full">
-                    hi
+                    <Scatter bind:chart data={scatter_data} options={{ responsive: true }} />
                 </div>
             {/if}
         </div>
